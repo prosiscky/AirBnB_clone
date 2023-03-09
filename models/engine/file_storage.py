5494a980d54ddbb3f4a9bb550833e81e10b13e49
prosiscky <legendaryprosperity001@gmail.com>
2023-03-09 14:35:12 -0800

    place class etc..

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-""" A Class that Serializes and Deserializes Objects"""
+"""This module contains the Filetorage class"""
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
