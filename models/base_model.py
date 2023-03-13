f5190a697571bd62a44d22f7af630d18d045d408
alishakor <aliuokeowo@gmail.com>
2023-03-13 01:19:08 +0100

    Basemodel modification

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,16 @@ class BaseModel:
     """Definition of the 'BaseModel' class"""
 
     def __init__(self, *args, **kwargs):
-        """This is the instance constructor"""
-
+        """The initialization function
+            Args:
+                    created_at: datetime - assign with the current
+                    datetime when an instance is created
+                    updated_at: datetime - assign with the current datetime
+                    when an instance is created and it will be updated
+                    every time you change your object
+                    id: string - assign with an uuid when an instance
+                    is created
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
